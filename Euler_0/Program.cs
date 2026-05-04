45a147025fa90c311b455aef8a4c67c50afdc83b
Kieran Edge <kesedge94@gmail.com>
2026-05-04 21:41:10 +0100

    Initial commit

---
+++ Euler_0/Program.cs
@@ -0,0 +1,13 @@
+﻿// Getting the sum of all odd square numbers
+long sum = 0;
+int max = 445000;
+
+for (long i = 1; i <= max; i++)
+{
+    if (i % 2 != 0)
+    {
+        sum += i * i;
+    }
+}
+
+Console.WriteLine(sum);
---
+++ Euler_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Initialise the list and return the last value
+List<long> fibonacciNumbers = new List<long> { 1, 2, 3 };
+long mostRecentNumber = fibonacciNumbers.Last();
+
+while (mostRecentNumber < 4000000)
+{
+    int listLength = fibonacciNumbers.Count;
+    long lastNumber = fibonacciNumbers.Last();
+    long numberBefore = fibonacciNumbers[listLength - 2];
+
+    mostRecentNumber = lastNumber + numberBefore;
+    if (mostRecentNumber <= 4000000)
+    {
+        fibonacciNumbers.Add(mostRecentNumber);
+    }
+}
+
+long sum = 0;
+foreach (long number in fibonacciNumbers)
+{
+    if (number % 2 == 0)
+    {
+        sum += number;
+    }
+}
+
+Console.WriteLine(sum);
