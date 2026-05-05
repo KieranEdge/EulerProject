ce75293491718170c384cb7a0d823022d30f14d9
Kieran Edge <kesedge94@gmail.com>
2026-05-05 22:52:09 +0100

    Euler Part 5 completed

---
+++ Euler_5/Helpers/PrimeFactorization.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler_5.Helpers
+{
+    internal static class PrimeFactorization
+    {
+        public static List<int> ReturnPrimeFactors(int number)
+        {
+            List<int> factors = new List<int>();
+
+            while (number % 2 == 0)
+            {
+                factors.Add(2);
+                number /= 2;
+            }
+
+            for (int i = 3; i * i <= number; i += 2)
+            {
+                while (number % i == 0)
+                {
+                    factors.Add(i);
+                    number /= i;
+                }
+            }
+
+            if (number > 1)
+            {
+                factors.Add(number);
+            }
+            return factors;
+        }
+
+        public static List<int> PrimesInRange(int start, int end)
+        {
+            List<int> primes = new List<int>();
+            for(int i = start; i <= end; i++)
+            {
+                if (isPrime(i)) primes.Add(i);
+            }
+            return primes;
+        }
+        public static bool isPrime(int number)
+        {
+            if (number == 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            for (int i = 3; i * i <= number; i += 2)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Euler_5/Program.cs
@@ -0,0 +1,61 @@
+﻿using Euler_5;
+using Euler_5.Helpers;
+
+int min = 2;
+int max = 20;
+
+// Reference list for counting primes
+List<int> primesInRange = PrimeFactorization.PrimesInRange(min, max);
+
+// List of lists for prime factors
+List<List<int>> primeFactorsInAllRange = new List<List<int>>();
+
+// Creating a list of prime factors for every value in the range
+for (int i = min;  i <= max; i++)
+{
+    List<int> primeFactors = PrimeFactorization.ReturnPrimeFactors(i);
+    primeFactorsInAllRange.Add(primeFactors);
+}
+
+// Creating a dictionary for the number of times each prime appears at most
+Dictionary<int, int> primesMaxMultiplicity = new Dictionary<int, int>();
+
+// Iterating over each prime number
+foreach (int primeFactor in primesInRange)
+{
+    // Counting the max occurence of each prime factor
+    int maxNumber = 0;
+
+    // Iterating over each list of prime factors
+    foreach(List<int> listOfPrimeFactors in primeFactorsInAllRange)
+    {
+        // Counting the occurence of the prime in that list
+        int counter = 0;
+        foreach(int factor in listOfPrimeFactors)
+        {
+            if(factor == primeFactor)
+            {
+                counter++;
+            }
+        }
+
+        if(counter > maxNumber)
+        {
+            maxNumber = counter;
+        }
+    }
+
+    primesMaxMultiplicity.Add(primeFactor, maxNumber);
+}
+
+double sum = 1;
+
+foreach (var item in primesMaxMultiplicity)
+{
+    double baseValue = item.Key;
+    double powerValue = item.Value;
+
+    sum *= Math.Pow(baseValue, powerValue);
+}
+
+Console.WriteLine($"Lowest Common Multiple: {sum}");
