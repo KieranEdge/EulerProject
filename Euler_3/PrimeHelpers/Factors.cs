74d1e42b8723930f4f810f5bb51dc252e9200cc8
Kieran Edge <kesedge94@gmail.com>
2026-05-05 21:11:45 +0100

    Problem 3 complete

---
+++ Euler_3/PrimeHelpers/Factors.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Euler_3.PrimeHelpers
+{
+    internal static class Factors
+    {
+        public static List<long> PrimeFactors(long number)
+        {
+            List<long> factors = new List<long>();
+
+            // Handle factor 2 separately
+            while (number % 2 == 0)
+            {
+                factors.Add(2);
+                number /= 2;
+            }
+
+            // Now check only odd numbers
+            for (long i = 3; i * i <= number; i += 2)
+            {
+                while (number % i == 0)
+                {
+                    factors.Add(i);
+                    number /= i;
+                }
+            }
+
+            // If anything remains, it's prime
+            if (number > 1)
+            {
+                factors.Add(number);
+            }
+
+            return factors;
+        }
+    }
+}
---
+++ Euler_3/Program.cs
@@ -0,0 +1,13 @@
+﻿using Euler_3.PrimeHelpers;
+
+long startingNumber = 600851475143;
+
+List<long> initialFactors = Factors.PrimeFactors(startingNumber);
+
+foreach (long factor in initialFactors)
+{
+    Console.WriteLine(factor);
+}
+long sum = initialFactors.Sum();
+
+Console.WriteLine(sum);
