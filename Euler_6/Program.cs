a73b5adb83454f8dab389b89084e267a88a3ea62
Kieran Edge <kesedge94@gmail.com>
2026-05-06 22:23:15 +0100

    Euler Part 6

---
+++ Euler_6/Program.cs
@@ -0,0 +1,24 @@
+﻿int min = 1;
+int max = 100;
+
+int sumOfNumbers = 0;
+
+// Getting the sum of the range
+for (int i = min; i <= max; i++)
+{
+    sumOfNumbers += i;
+}
+
+// Getting the square of this value
+long squareOfRangeSum = sumOfNumbers * sumOfNumbers;
+
+// Getting the sum of all the squares
+long sumOfIndividualNumbersSquared = 0;
+
+for (int i = min; i <= max; ++i)
+{
+    sumOfIndividualNumbersSquared += (i * i);
+}
+
+// Getting the difference
+Console.WriteLine(squareOfRangeSum -  sumOfIndividualNumbersSquared);
