4476359d42e8b17b504556dc8b31f13c6ff8a779
Kieran Edge <kesedge94@gmail.com>
2026-05-07 20:29:11 +0100

    Part 9

---
+++ Euler_9/Helpers/Triples.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler_9.Helpers
+{
+    internal class Triples
+    {
+        public static List<double> GeneratePythagoreanTriples(int m, int n)
+        {
+            List<double> result = new List<double>();
+
+            if (m == n)
+            {
+                Console.WriteLine($"Factors cannot be the same");
+                return result;
+            }
+            else if (m < n)
+            {
+                Console.WriteLine("m must be greater than n");
+            }
+
+            double a = Math.Pow(m, 2) - Math.Pow(n, 2);
+            double b = 2 * m * n;
+            double c = Math.Pow(m, 2) + Math.Pow(n, 2);
+
+            result.Add(a);
+            result.Add(b);
+            result.Add(c);
+
+            return result;
+        }
+    }
+}
---
+++ Euler_9/Program.cs
@@ -0,0 +1,32 @@
+﻿using Euler_9.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+int maxSearchFactor = 20;
+List<double> matchingTriple = new List<double>();
+
+for(int i = 1; i <= maxSearchFactor; i++)
+{
+    for(int j = 1; j <= maxSearchFactor; j++)
+    {
+        if(j < i)
+        {
+            List<double> triples = Triples.GeneratePythagoreanTriples(i, j);
+
+            if(triples.Sum() == 1000)
+            {
+                matchingTriple = triples;
+                break;
+            }
+        }
+    }
+}
+
+if(matchingTriple.Count > 0)
+{
+    Console.WriteLine("Matching trible found");
+    Console.WriteLine($"a = {matchingTriple[0]}");
+    Console.WriteLine($"b = {matchingTriple[1]}");
+    Console.WriteLine($"c = {matchingTriple[2]}");
+
+    Console.WriteLine($"Product of factors = {matchingTriple[0] * matchingTriple[1] * matchingTriple[2]}");
+}
