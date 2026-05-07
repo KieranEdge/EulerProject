eb0da100d7ae81cbcf5accea86409e976e5a7234
Kieran Edge <kesedge94@gmail.com>
2026-05-07 20:35:54 +0100

    Part 10

---
+++ Euler_10/Program.cs
@@ -0,0 +1,14 @@
+﻿using Euler_10.Helper;
+
+long max = 2000000;
+List<long> primes = new List<long>();
+
+for (long i = 2;  i <= max; i++)
+{
+    if (PrimeInformation.IsPrimeNumber(i))
+    {
+        primes.Add(i);
+    }
+}
+
+Console.WriteLine($"Sum of all primes under {max}: {primes.Sum()}");
