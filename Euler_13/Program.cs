b5e03e147302fe104a664883867eeee14933d961
Kieran Edge <kesedge94@gmail.com>
2026-05-10 17:47:15 +0100

    Part 13

---
+++ Euler_13/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+string filePath = "C:\\Users\\Kieran Edge\\source\\repos\\CSCourse\\ProjectEuler\\Euler_13\\Data.txt";
+string[] linesInFile = File.ReadAllLines(filePath);
+List<BigInteger> numbers = new List<BigInteger>();
+
+foreach(string line in linesInFile)
+{
+    numbers.Add(BigInteger.Parse(line));
+}
+
+BigInteger Sum = BigInteger.Zero;
+
+foreach(BigInteger number in numbers)
+{
+    Sum += number;
+}
+
+Console.WriteLine(Sum);
---
+++ Euler_14/Program.cs
@@ -0,0 +1,29 @@
+﻿int start = 1;
+int end = 1000000;
+int maxSequence = 0;
+
+for (int i = start; i < end; i++)
+{
+    Console.WriteLine($"Calculating for {i}");
+    int nextNumber = i;
+    int sequenceLength = 1;
+    while (nextNumber != 1)
+    {
+        if (nextNumber % 2 == 0)
+        {
+            nextNumber /= 2;
+        }
+        else
+        {
+            nextNumber = (nextNumber * 3) + 1;
+        }
+        sequenceLength++;
+    }
+    //Console.Write($"{i} has a sequence of {sequenceLength}");
+    if(sequenceLength > maxSequence)
+    {
+        maxSequence = sequenceLength;
+    }
+}
+
+Console.WriteLine($"Max sequence length = {maxSequence}");
