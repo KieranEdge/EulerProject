3b3325fad3978448b781862011d746a84166b7bb
kieranedge <kesedge94@gmail.com>
2026-05-10 20:43:58 +0100

    Part 16

---
+++ Euler_16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+BigInteger basePower = BigInteger.Pow(2, 1000);
+
+string powerValueAsString = basePower.ToString();
+
+List<int> individualDigits = new List<int>();
+
+foreach (char c in powerValueAsString)
+{
+    individualDigits.Add(int.Parse(c.ToString()));
+}
+
+int sum = 0;
+
+foreach (int d in individualDigits)
+{
+    sum += d;
+}
+
+Console.WriteLine(sum);
